fca03a0d6e298fa39157af1a641290fbd3a719e6
Turner Silverthorne <turner.silverthorne@gmail.com>
2023-10-22 18:48:25 -0400

    Figure updates

---
+++ plotConfig.R
@@ -0,0 +1,29 @@
+user='TurnerMac'
+
+if (user == 'TurnerMac'){
+	overleaf_plot_directory = '/Users/turnersilverthorn/research/overleaf/samplingPaper/figures/'
+}
+
+control_labels <- function(plot, 
+                           x_label = T, y_label = T, 
+                           x_ticks = T, y_ticks = T,
+                           legend_vis = T) {
+  if (!x_label) {
+    plot <- plot + theme(axis.title.x = element_blank())
+  }
+  
+  if (!y_label) {
+    plot <- plot + theme(axis.title.y = element_blank())
+  }
+  
+  if (!x_ticks) {
+    plot <- plot + theme(axis.text.x = element_blank())
+  }
+  
+  if (!y_ticks) {
+    plot <- plot + theme(axis.text.y = element_blank())
+  }
+  
+  #TODO legend_vis option
+  return(plot)
+}
