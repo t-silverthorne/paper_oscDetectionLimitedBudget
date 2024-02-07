e76bf79217136b13b58b1c3d0c9679490e093ec7
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-02-07 10:40:49 -0500

    option to regularize using L1 cost function added to all but CVXR

--- R/costfun_2lattice_svdpower.R
+++ R/costfun_2lattice_svdpower.R
@@ -1,7 +1,7 @@
 #' Helper function for continuous power optimization, restricted to two lattice subspace
 #' @export
 costfun_2lattice_svdpower=function(shift1,shift2,scale1,scale2,lat1,lat2,
-                                   freqs,Amp=1,alpha=.05){
+                                   freqs,Amp=1,alpha=.05,regL1=0){
   mt=convert_2lattice_to_state(shift1,shift2,scale1,scale2,lat1,lat2)
-  return(costfun_svdpower(mt,freqs,Amp,alpha))
+  return(costfun_svdpower(mt,freqs,Amp,alpha,regL1))
 }
--- R/costfun_svdpower_discrete.R
+++ R/costfun_svdpower_discrete.R
@@ -1,3 +1,3 @@
-costfun_svdpower_discrete = function(xinds,tau,freqs,Amp=1,alpha=.05){
-  return(costfun_svdpower(tau[xinds],freqs,Amp,alpha))
+costfun_svdpower_discrete = function(xinds,tau,freqs,Amp=1,alpha=.05,regL1=0){
+  return(costfun_svdpower(tau[xinds],freqs,Amp,alpha,regL1))
 }
