9d8033e79dcb75e117451e06e589fc41deeadc3f
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-02-11 11:18:06 -0500

    regL1 now in ggtitle, LFS update, allfigs renamed

--- figs_for_paper/fig_gen_wrapper.R
+++ figs_for_paper/fig_gen_wrapper.R
@@ -4,5 +4,5 @@ files_to_run=c('results/output/Nmeas16/Nfi_288_Nfr_64_rL1_0_msa_1000_mbf_10_nrep
 for (f in files_to_run){
   print(paste0('Running: ',f))
   write(f,file='figs_for_paper/fnames.csv')
-  rmarkdown::render('figs_for_paper/allfigs.Rmd',knit_root_dir = getwd())
+  rmarkdown::render('figs_for_paper/makeFig1Fig2.Rmd',knit_root_dir = getwd())
 }
