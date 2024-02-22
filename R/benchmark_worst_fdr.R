5619b0d26de41d3e64f047659dff00f6b1757bd4
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-02-22 16:21:11 -0500

    Added option to specify FDR method, minor changes to motivation figure

--- R/benchmark_worst_fdr.R
+++ R/benchmark_worst_fdr.R
@@ -1,4 +1,5 @@
-benchmark_worst_fdr=function(mt,Amp,f0,Nmc,Nacro,Nfreq,fmin,fmax){
+benchmark_worst_fdr=function(mt,Amp,f0,Nmc,Nacro,Nfreq,fmin,fmax,
+                             fdr_method='BH'){
     freqs = seq(from=fmin,to=fmax,length.out=Nfreq)
     acros = seq(from=0,to=2*pi,length.out=Nacro)
     
@@ -12,7 +13,7 @@ benchmark_worst_fdr=function(mt,Amp,f0,Nmc,Nacro,Nfreq,fmin,fmax){
       pmat=Lfit%>% rbindlist() %>% as.matrix() %>% t()
       qmat=matrix(replicate(Nmc*Nfreq,{NaN}),nrow=Nmc)
       for (ii in c(1:Nmc)){
-        qmat[ii,]=p.adjust(pmat[ii,],'BY')
+        qmat[ii,]=p.adjust(pmat[ii,],fdr_method)
       }
       is_osc = apply(qmat,1,function(x){any(x<.05)}) 
       if (length(is_osc)==Nmc){
