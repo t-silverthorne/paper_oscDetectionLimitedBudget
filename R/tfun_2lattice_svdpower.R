78a33af24db9d1035cfb2bd147da777915e305d9
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-02-29 15:18:12 -0500

    Removing outdated 2-lattice transition function, and other partially supported code

--- R/tfun_2lattice_svdpower.R
+++ R/tfun_2lattice_svdpower.R
@@ -16,18 +16,6 @@ if(control$tfun_choice=='unif-with-bdry'){
               shift2=shift2)) 
 }else if (control$tfun_choice=='default'){ # keeps shift1=0
   stop('Reflecting boundaries not yet tested/implemented')
-  # generate random numbers
-  rnd_scale1 = rnorm(1,control$mean_scale1,control$sd_scale1)  
-  rnd_scale2 = rnorm(1,control$mean_scale2,control$sd_scale2)
-  rnd_shift2 = rnorm(1,control$mean_shift2,control$sd_shift2)
- 
-  scale1     = reflecting_rescale_cts2lattice(shift1,scale1,rnd_scale1,lat1)
-  scale2     = reflecting_rescale_cts2lattice(shift2,scale2,rnd_scale2,lat2) 
-  shift2     = reflecting_reshift_cts2lattice(shift2,scale2,rnd_shift2,lat2)
-  
-  return(list(scale1=scale1,
-              scale2=scale2,
-              shift2=shift2)) 
 }else{
   stop('unknown control$tfun_choice')
   
--- R/tfun_2lattice_svdpower_discrete.R
+++ R/tfun_2lattice_svdpower_discrete.R
@@ -12,37 +12,7 @@ tfun_2lattice_svdpower_discrete=function(N1,dx1,N2,dx2,xshift2,Nfine,control){
       }
     }
   }else if (control$tfun_choice=='reflecting-brownian'){
-    stop('unstable and untested method, verify manually before using')
-    accept_state=F
-    while (accept_state==F){
-      ddx1    = sample(c(-1,0,1),1)
-      ddx2    = sample(c(-1,0,1),1)
-      dshift2 = sample(c(-1,0,1),1) 
-    
-      if (dx1+ddx1 < 1 |(dx1+ddx1)*(N1-1)>Nfine){
-        dx1_new = dx1-ddx1 
-      }else{
-        dx1_new = dx1+ddx1 
-      }
-      
-      if (dx2+ddx2 < 1 |xshift2 + (dx2+ddx2)*(N2-1)>Nfine){
-        dx2_new = dx2-ddx2 
-      }else{
-        dx2_new = dx2+ddx2  #TODO: check this is exhaustive
-      }
-    
-      if( xshift2 + dshift2>0 &(xshift2+dshift2 + (dx2_new)*(N2-1)<=Nfine)){
-        xshift2_new = xshift2 + dshift2
-      }else if( xshift2 - dshift2>0 &(xshift2-dshift2 + (dx2_new)*(N2-1)<=Nfine)){
-        xshift2_new = xshift2 - dshift2
-      }else{
-        xshift2_new = xshift2  
-      } 
-      if(!anyDuplicated(xinds_from_lat1lat2_pars(N1,dx1_new,N2,dx2_new,xshift2_new))){
-        accept_state = T
-      }
-    }
-    
+    stop('reflecting-brownian is depracated')
   }else{
     stop('unknown control$tfun_choice')
   }
