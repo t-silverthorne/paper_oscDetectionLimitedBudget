7d45b03d53ebe43bad0ae9b1c2835a684d8ed537
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-02-29 16:06:46 -0500

    Consolidating solvers to one file

--- R/helpers.R
+++ R/helpers.R
@@ -93,6 +93,42 @@ helper_gap_penalty=function(mt){
   return(max(c(diff(mt),d1)))
 }
 
+xinds_from_lat1lat2_pars=function(N1,dx1,N2,dx2,xshift2){
+  lat1  = 1+dx1*c(0:(N1-1)) 
+  lat2  = 1+xshift2 + dx2*c(0:(N2-1))
+  xinds = c(lat1,lat2)
+  return(xinds)
+}
+
+wrapper_sweep_lattice=function(optim_routine,Nvals,Nmeas,cts_flag,dvar0,...){
+  fval_best = Inf
+  res_best  = NULL
+  N1_best   = NULL
+  time_tot  = 0
+  all_fvals = NULL 
+  for (n1 in Nvals){
+    n2=Nmeas-n1
+    if (cts_flag){
+      dvar0[['lat1']]= c(1:n1)/n1 - 1/n1
+      dvar0[['lat2']]= c(1:n2)/n2 - 1/n2
+      res_loc = optim_routine(dvar0,...)
+    }else{
+      dvar0$N1=n1
+      dvar0$N2=n2
+      res_loc = optim_routine(dvar0,...)
+    }
+    time_tot=time_tot+as.numeric(res_loc$runtime)
+    all_fvals=rbind(all_fvals,data.frame(fval=res_loc$fvalue,N1=n1))
+    if (res_loc$fvalue<fval_best){
+      res_best  = res_loc
+      N1_best   = n1
+      fval_best = res_loc$fvalue
+    }
+  }
+  return(list(res_best=res_best,N1_best=N1_best,
+              time_tot=time_tot,
+              all_fvals=all_fvals))
+}
 
 helper_extract_info_from_result=function(res,tag,Nmeas=gset$Nmeas,runtime_tot,cfunlabel='power',...){
   tagsp=strsplit(tag,'_')
@@ -141,3 +177,5 @@ helper_update_amm=function(amm,Lnow,gset,tag,wrapped=F,...){
   }
   return(amm)  
 }
+
+
