f8405059b98e9fbc94e846f959f66b1ff316d5e9
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-04-12 11:29:05 -0400

    Added L2 freq/phase derivative regularisation to Gurobi code

--- matlab_checks/reg_term_test.m
+++ matlab_checks/reg_term_test.m
@@ -5,7 +5,12 @@ c2fun   = @(t) cos(2*pi*f*t - phi)^2;
 dc2dphi = @(t) diff(c2fun(t),phi);
 dc2df   = @(t) diff(c2fun(t),f);
 
-% Exact expression for L2 norm with uniform prior
+% exact expresssion L2norm squraed -- off diagonal terms 
 phi_int = int(dc2dphi(ti)*dc2dphi(tj) + dc2df(ti)*dc2df(tj),phi,0,2*pi);
+f_int = int(phi_int,f,fmin,fmax);
+simplify(f_int)
+
+% exact expresssion L2norm squraed -- on diagonal terms 
+phi_int = int(dc2dphi(t)*dc2dphi(t) + dc2df(t)*dc2df(t),phi,0,2*pi);
 f_int = int(phi_int,f,fmin,fmax);
 simplify(f_int)
