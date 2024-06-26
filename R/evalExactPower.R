f257c7ebbfe23611f7c12db221d66faf0bc31405
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-06-26 19:06:03 -0400

    Updates to source

--- R/evalExactPower.R
+++ R/evalExactPower.R
@@ -17,15 +17,27 @@
 #' @return statistical power 
 #' @author Turner Silverthorne
 #' @export
-evalExactPower <- function(t,param,alpha=.05){
+evalExactPower <- function(t,param,alpha=.05,method='schur'){
 # return power of one-frequency cosinor model
   Amp    = param[['Amp']]
   freq   = param[['freq']]
   acro   = param[['acro']]
   N      = length(t)
   
-  cvec   = Amp*cos(2*pi*freq*t-acro)
-  lambda = as.numeric(t(cvec)%*%cvec)
+  if (method=='schur'){
+    A      = matrix(c(0,0,1,0,0,1),nrow=3,byrow=T)
+    Xr     = matrix(c(cos(2*pi*freq*t),sin(2*pi*freq*t)),ncol=2)
+    D      = t(Xr)%*%Xr
+    b      = matrix(c(sum(cos(2*pi*freq*t)),sum(sin(2*pi*freq*t))),ncol=1)
+    invB   = D - b%*%t(b)/N
+    beta   = matrix(c(Amp*cos(acro),Amp*sin(acro)),nrow=2)
+    lambda = t(beta)%*%invB%*%beta
+  }else if (method=='old'){
+    cvec   = Amp*cos(2*pi*freq*t-acro)
+    lambda = as.numeric(t(cvec)%*%cvec)
+  }else{
+    stop('unknown method')
+  }
   
   f0     = qf(p=1-alpha,df1=2,df2=N-3)
   return(1 - pf(q=f0,df1=2,df2=N-3,ncp=lambda))
